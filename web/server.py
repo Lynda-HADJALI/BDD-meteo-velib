04c85812ca7a7d172987129fe244baf728a7b47f
anis2899 <anissmati074@gmail.com>
2022-01-21 14:12:05 +0100

    update

--- web/server.py
+++ web/server.py
@@ -1,5 +1,5 @@
 import json
-
+import sqlite3
 from flask import Flask, render_template, request, session, url_for, redirect
 
 
@@ -12,14 +12,14 @@ def loadCom():
          return listOfCom
 
 def create_app(config): 
-    app = Flask(__name__)
+    app = Flask(__name__ )
     app.secret_key = 'something_special'
     app.config.from_object("config")
     app.config["TESTING"] = config.get("TESTING")
 
 
     coms = loadCom()
-    
+
     @app.route('/')
     def index():
         return render_template('index.html')
@@ -27,9 +27,18 @@ def create_app(config):
     @app.route('/showSummary',methods=['POST'])
     def showSummary():
         com = [com for com in coms if com == request.form['com']]
+
+        '''conn = sqlite3.connect('data.db')
+
+        cur = conn.cursor()
+
+        req = "select * from velib2"
+        result = cur.execute(req)'''
         if com:
             com = [com for com in coms if com == request.form['com']][0]
-            return render_template('welcome.html', com=com)
+            req = "select * from velib2 where "
+            result = cur.execute(req)
+            return render_template('welcome.html', result)
         else :
             error='Desole, cette communes ne contient pas de velib'
             return render_template('index.html', error=error)
@@ -41,11 +50,11 @@ def create_app(config):
     @app.route('/logout')
     def logout():
         return redirect(url_for('index'))
-    
+
     return app
 
 app = create_app({"TESTING": False})
 
 
-if __name__ == "__main__":
+if __name__ == "main":
     app.run(debug=True)
